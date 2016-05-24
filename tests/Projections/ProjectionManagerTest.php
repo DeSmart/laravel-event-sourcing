8b58bdb714552010a1194bba5bebcf6cd2f0838d
Michal Golon <gollum@desmart.com>
2016-05-24 12:58:15 +0200

    Refactors ProjectionManager test file.

--- tests/Projections/ProjectionManagerTest.php
+++ tests/Projections/ProjectionManagerTest.php
@@ -10,19 +10,49 @@ use stubs\ProjectionStub;
 class ProjectionManagerTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var ProjectionManager
+     */
+    protected $projectionManager;
+
+    /**
+     * @var ProjectionStub
+     */
+    protected $projection;
+
+    public function setUp()
+    {
+        $this->projection = new ProjectionStub();
+        $this->projectionManager = new ProjectionManager();
+    }
+
+    /**
      * @test
      */
     public function it_registers_and_notifies_projection()
     {
-        $projection = new ProjectionStub();
         $eventStream = new EventStream(...[new PointsWereAdded(100)]);
 
-        $this->assertEquals(0, $projection->getPoints());
-        
-        $manager = new ProjectionManager();
-        $manager->registerProjection($projection);
-        $manager->notify($eventStream);
-        
-        $this->assertEquals(100, $projection->getPoints());
+        $this->assertEquals(0, $this->projection->getPoints());
+
+        $this->projectionManager->registerProjection($this->projection);
+        $this->projectionManager->notify($eventStream);
+        $this->assertEquals(100, $this->projection->getPoints());
+    }
+
+    /**
+     * @test
+     */
+    public function it_clears_projection()
+    {
+        $eventStream = new EventStream(...[new PointsWereAdded(100)]);
+
+        $this->assertEquals(0, $this->projection->getPoints());
+
+        $this->projectionManager->registerProjection($this->projection);
+        $this->projectionManager->notify($eventStream);
+        $this->assertEquals(100, $this->projection->getPoints());
+
+        $this->projectionManager->clear();
+        $this->assertEquals(0, $this->projection->getPoints());
     }
 }
